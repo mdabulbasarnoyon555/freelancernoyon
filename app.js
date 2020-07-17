c9b557e8d3313594e1e605199595edf236c8d165
mdabulbasarnoyon555 <68434689+mdabulbasarnoyon555@users.noreply.github.com>
2020-07-17 21:36:55 +0600

    my personal website

---
+++ app.js
@@ -0,0 +1,9 @@
+function myImagegallery() {
+  document.getElementById("ImageGellery").innerHTML = "Paragraph changed.";
+}
+
+
+// document.addEventListener ("contextmenu" , function (FreelancerNoyon){
+// FreelancerNoyon.preventDefault();
+
+// });
